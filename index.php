f3620d5ab77be01235b94ca294fa3056bb9a8274
Luigi <luiginigrelli@me.com>
2019-07-09 14:40:11 +0200

    Named params

--- index.php
+++ index.php
@@ -28,11 +28,24 @@
     }
     echo "</p>";
 
-    /** Prepared Statements */
+    /** Prepared Statements: Positional Parameters */
     $sql = "SELECT * FROM products WHERE id = ?";
     $stmt = $pdo->prepare($sql);
     $id = 1;
     $stmt->execute([$id]);
+    $products = $stmt->fetchAll(PDO::FETCH_OBJ);
+    echo "<p>";
+    foreach($products as $product){
+        echo "$product->title <br/>";
+    }
+    echo "</p>";
+
+    /** Prepared Statements: Named Parameters */
+    $sql = "SELECT * FROM products WHERE id = :id AND category_id = :category_id";
+    $stmt = $pdo->prepare($sql);
+    $id = 2;
+    $category_id = 1;
+    $stmt->execute(['id'=>$id, 'category_id'=>$category_id]);
     $products = $stmt->fetchAll(PDO::FETCH_OBJ);
     echo "<p>";
     foreach($products as $product){
