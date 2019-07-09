6df00775c778750a9be9b93939af59803fe220de
Luigi <luiginigrelli@me.com>
2019-07-09 14:17:16 +0200

    PDO Query example

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
 <?php
 
+    // DB Params
+    $host = "localhost";
+    $db_name = "test-api";
+    $username = "test";
+    $password = "123456";
+
+    // Set DNS
+    $dns = "mysql:host=$host;dbname=$db_name";
+
+    // Create PDO instance
+    $pdo = new PDO($dns, $username, $password);
+
+    /** PDO QUERY */
+    $stmt = $pdo->query("SELECT * FROM products");
+
+    echo "<p>";
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        echo $row['title'] . "<br/>";
+    }
+    echo "</p>";
+
+    $stmt = $pdo->query("SELECT * FROM products");
+    echo "<p>";
+    while($row = $stmt->fetch(PDO::FETCH_OBJ)){
+        echo $row->title . "<br/>";
+    }
+    echo "</p>";
 ?>
